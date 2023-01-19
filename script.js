cc280f7eaa1e29e86c8e4f07d5c40cd5c6cdd9e1
Cynthia-Isutsa <isutsacynthia@gmail.com>
2023-01-19 06:35:05 +0300

    toggle button

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 //select all the elements
-var toggle_btn = document.querySelector(".toggle-btn")
-var big_wrapper =  document.querySelector(".big-wrapper")
+var toggle_btn
+var big_wrapper 
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn")
+    big_wrapper = document.querySelector(".big-wrapper")
+}
+
 const main = document.querySelector("main")
+declare()
+
+let dark = false;
 
 function toggleAnimation(){
     //clone the wrapper
+    dark= !dark
     let clone = big_wrapper.cloneNode(true)
+    if(dark == true){
+        clone.classList.remove("light")
+        clone.classList.add("dark")
+    }else{
+        clone.classList.remove("dark")
+        clone.classList.add("light")
+    }
+    clone.classList.add("copy")
     main.appendChild(clone)
-}
 
+    clone.addEventListener("animationend", () =>{
+        big_wrapper.remove()
+        clone.classList.remove("copy")
+        //reset variable
+        declare()
+        events()
+    })
+}
+function events(){
 toggle_btn.addEventListener("click", toggleAnimation)
+
+}
+events()
